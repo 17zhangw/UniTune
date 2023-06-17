5addd52a9a4de1d6d2737b35330e033c37e9fff9
William Zhang <17zhangw@gmail.com>
2023-06-17 01:02:21 +0000

    Fix

--- main.py
+++ main.py
@@ -3,12 +3,17 @@ from MultiTune.advisor.alternative_adviser import TopAdvisor
 from MultiTune.utils.parser import parse_args
 from MultiTune.database.postgresdb import PostgresDB
 
+import argparse
 
 
 
 if __name__ == '__main__':
     if __name__ == '__main__':
-        args_db, args_tune = parse_args()
+        prog = argparse.ArgumentParser(prog="UniTune")
+        prog.add_argument("--config-ini", required=True)
+        a = prog.parse_args()
+
+        args_db, args_tune = parse_args(a.config_ini)
         db = PostgresDB(args_tune['task_id'], **args_db)
         top_adviser = TopAdvisor(db, args_tune)
         print("Run {}\n".format(args_tune['arm_method']))
